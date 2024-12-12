689c16b34e8677c189e2ed35fa0880481bf5fdfc
Thomas Wiesner <485781+tomw1808@users.noreply.github.com>
2024-12-12 14:41:48 +0100

    get variables from environment variables

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@ import { keccak256, toHex } from 'viem';
 
 // BTC market
 const MARKET_ID = keccak256(toHex(process.env.MARKET_ID || "CRYPTO_BTC"));
-const LEVERAGE = 10.0
-const MPH_TOKENS = 5
-const MOVING_AVERAGE_PERIOD = 5 // 5 minutes
-const THRESHOLD_PERCENTAGE = 0.1 // Open position if price is over / under 0.1% of moving average
+const LEVERAGE = Number(process.env.LEVERAGE || 10.0)
+const MPH_TOKENS = Number(process.env.MPH_TOKENS || 5)
+const MOVING_AVERAGE_PERIOD = Number(process.env.MOVING_AVERAGE_PERIOD || 5) // 5 minutes
+const THRESHOLD_PERCENTAGE = Number(process.env.THRESHOLD_PERCENTAGE || 0.1) // Open position if price is over / under 0.1% of moving average
 
-if(process.env.PRIVATE_KEY == undefined) {
+if (process.env.PRIVATE_KEY == undefined) {
     console.error("Private Key is not set, aborting");
     process.exit(1)
 }
